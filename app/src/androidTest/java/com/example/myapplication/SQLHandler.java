6590fa17575e38c1a6859e788dba5e87f7041f3c
Lindsay Pan <lpan0010@student.monash.edu>
2019-03-16 13:51:01 +1100

    imported classes

--- app/src/androidTest/java/com/example/myapplication/SQLHandler.java
+++ app/src/androidTest/java/com/example/myapplication/SQLHandler.java
@@ -1,5 +1,12 @@
 package com.example.myapplication;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.Cursor;
+import android.content.Context;
+import android.content.ContentValues;
+
+
 public class SQLHandler {
 
 }
