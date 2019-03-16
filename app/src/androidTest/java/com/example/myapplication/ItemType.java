21325417e49d2502fda8f015dc80fc9f53cd6ce5
Lindsay Pan <lpan0010@student.monash.edu>
2019-03-16 13:54:17 +1100

    added methods to sqlhepler

--- app/src/androidTest/java/com/example/myapplication/ItemType.java
+++ app/src/androidTest/java/com/example/myapplication/ItemType.java
@@ -1,5 +1,33 @@
 package com.example.myapplication;
 
 public class ItemType {
-    
+
+    private int ID;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+
+    public String getMessage() {
+        return Message;
+    }
+
+    public void setMessage(String message) {
+        Message = message;
+    }
+
+    private String Type;
+    private String Message; //message to optimise the carbon emission
 }
--- app/src/androidTest/java/com/example/myapplication/SQLHandler.java
+++ app/src/androidTest/java/com/example/myapplication/SQLHandler.java
@@ -7,6 +7,24 @@ import android.content.Context;
 import android.content.ContentValues;
 
 
-public class SQLHandler {
+public class SQLHandler extends SQLiteOpenHelper {
 
+
+
+
+
+    
+    public SQLHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
